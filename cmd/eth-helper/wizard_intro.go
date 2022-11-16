32d6d7bc971ca0598a8e7768a1856d50aef6c13d
95decode <95decode@gmail.com>
2022-11-16 18:00:20 +0900

    Add create account sample

--- cmd/eth-helper/wizard_intro.go
+++ cmd/eth-helper/wizard_intro.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+
+	keystore "github.com/ethereum/go-ethereum/accounts/keystore"
 )
 
 // makeWizard creates and returns a new puppeth wizard.
@@ -20,7 +22,7 @@ func (w *wizard) run() {
 	for {
 		fmt.Println()
 		fmt.Println("What would you like to do? (default = 1)")
-		fmt.Println(" 1. foo")
+		fmt.Println(" 1. Create account")
 		fmt.Println(" 2. bar")
 		fmt.Println(" 3. flag test")
 
@@ -28,6 +30,15 @@ func (w *wizard) run() {
 		switch {
 		case choice == "" || choice == "1":
 			fmt.Println("fooooooo")
+
+			_, ks := tmp(true)
+
+			a, err := ks.NewAccount("foo")
+			if err != nil {
+				fmt.Println("error 1")
+			}
+
+			fmt.Println(a.Address)
 		case choice == "2":
 			fmt.Println("baaaaaar")
 		case choice == "3":
@@ -36,4 +47,13 @@ func (w *wizard) run() {
 			fmt.Println("That's not something I can do")
 		}
 	}
+}
+
+func tmp(encrypted bool) (string, *keystore.KeyStore) {
+	d := "./"
+	newKs := keystore.NewPlaintextKeyStore
+	if encrypted {
+		newKs = func(kd string) *keystore.KeyStore { return keystore.NewKeyStore(kd, 2, 1) }
+	}
+	return d, newKs(d)
 }
