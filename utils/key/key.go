2bdca8dfc17ef1d922b10fe31693215ab15e33ba
95decode <95decode@gmail.com>
2022-11-17 18:58:04 +0900

    Add key module

---
+++ utils/key/key.go
@@ -0,0 +1,14 @@
+package key
+
+import (
+	keystore "github.com/ethereum/go-ethereum/accounts/keystore"
+)
+
+func GenerateKey(encrypted bool) (string, *keystore.KeyStore) {
+	d := "./keys"
+	newKs := keystore.NewPlaintextKeyStore
+	if encrypted {
+		newKs = func(kd string) *keystore.KeyStore { return keystore.NewKeyStore(kd, 2, 1) }
+	}
+	return d, newKs(d)
+}
