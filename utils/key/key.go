1550c1097244521eed4f33ceecc1952b3e163be5
95decode <95decode@gmail.com>
2022-11-22 11:03:24 +0900

    Change N parameter

--- utils/key/key.go
+++ utils/key/key.go
@@ -4,11 +4,18 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 )
 
+const (
+	StandardScryptN = 1 << 18
+	StandardScryptP = 1
+)
+
 func GenerateKey(encrypted bool) (string, *keystore.KeyStore) {
 	d := "./keys"
 	newKs := keystore.NewPlaintextKeyStore
+
 	if encrypted {
-		newKs = func(kd string) *keystore.KeyStore { return keystore.NewKeyStore(kd, 2, 1) }
+		newKs = func(kd string) *keystore.KeyStore { return keystore.NewKeyStore(kd, StandardScryptN, StandardScryptP) }
 	}
+
 	return d, newKs(d)
 }
